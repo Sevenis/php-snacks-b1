d86f480c9832aa081961ba3766885cc1868adf7e
Pietro Sette <pietrosette@gmail.com>
2020-09-16 22:36:17 +0200

    Snack commentato

--- snack1.php
+++ snack1.php
@@ -1,3 +1,8 @@
+<!-- Passare come parametri GET name, mail ed age e verificare (cercando i metodi che
+non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail
+contenga un punto e una chiocciola e che age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
 <?php
 
 $nome = $_GET['name'];
